4bbf812fa360a040bff49ae1f0486c48277d72f1
Osvaldo Colina <osvaldo.colina@gmail.com>
2020-05-16 22:36:00 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## elpais-scraper ##
+
+
---
+++ index.js
@@ -0,0 +1,98 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+const BASE_URL = 'https://www.elpais.com.uy';
+const CONFIG = { headless: true };
+const UNSUPPORTED_SECTIONS = ['especiales'];
+
+const getLinks = async () => {
+    const browser = await puppeteer.launch(CONFIG);
+
+    try {
+        const page = await browser.newPage();
+
+        await page.goto(BASE_URL);
+        await page.evaluate('document.documentElement.webkitRequestFullscreen()');
+        await page.waitForSelector('.default-menu');
+
+        const links = await page.evaluate(
+            () => Array.from(
+                document.querySelectorAll('[class="page-link"]'),
+                a => a.getAttribute('href')
+            )
+        );
+
+        const supportedLinks = links.
+            filter((link) =>
+                !UNSUPPORTED_SECTIONS.includes(link.split('/')[1])
+            );
+
+        return Array.from(new Set(supportedLinks));
+
+    } catch (error) {
+        console.log(error);
+
+    } finally {
+        await browser.close();
+    }
+};
+
+const getArticle = async (path) => {
+    const browser = await puppeteer.launch(CONFIG);
+
+    try {
+        const PAGE_URL = `${BASE_URL}/${path}`;
+        const page = await browser.newPage();
+
+        await page.goto(PAGE_URL, { timeout: 60000 });
+        await page.evaluate('document.documentElement.webkitRequestFullscreen()');
+        await page.waitForSelector('[class="hamburger-menu-icon"]', { timeout: 3000 });
+
+        const article = await page.evaluate(() => {
+            const title = document.querySelector('h1.title').textContent;
+            const subtitle = document.querySelector('h2.epigraph').textContent;
+            const image = document.querySelector('[class="article-content"] figure img').getAttribute('src');
+            const paragraphs = document.querySelectorAll('.paragraph');
+            let content = [];
+
+            for (const paragraph of paragraphs) {
+                const currentContent = paragraph.textContent;
+                content.push(currentContent);
+            }
+
+            return { title, subtitle, image, content };
+        });
+
+        return article;
+
+    } catch (error) {
+        console.log(error);
+        console.log('Link: ', `${BASE_URL}/${path}`);
+
+    } finally {
+        await browser.close();
+    }
+};
+
+const getArticles = async () => {
+    console.log('Start: ', new Date);
+
+    const links = await getLinks();
+    console.log('# of Links: ', links.length);
+
+    let articles = [];
+
+    for (const link of links) {
+        const article = await getArticle(link);
+        articles.push(article);
+    }
+
+    console.log('# of Articles: ', articles.length);
+
+    const result = { date: new Date(), articles };
+
+    fs.writeFileSync('article.json', JSON.stringify(result));
+    console.log('Finish: ', new Date);
+};
+
+getArticles();
